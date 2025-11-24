a4d32d4c0c7417bd9f33c29c9a25ea703995e105
Delano Sewell <delanosewelledu@gmail.com>
2025-11-23 23:59:21 -0500

    Completed Requirements for Project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", () =>{
+    const form = document.querySelector(".newsletter form");
+    const eInput = document.querySelector("#email");
+    const messageBox= document.querySelector(".message");
+    
+    form.addEventListener("submit", (event) =>
+                         {
+        event.preventDefault();
+        
+        const email =eInput.value.trim();
+        
+        if(email==="") {
+            messageBox.textContent = "Please enter a valid email address.";
+            return;
+        }
+        messageBox.textContent=
+            `Thank you! Your email address ${email} has been added to our mailing list!`;        
+        eInput.value = "";
+    });
+});
